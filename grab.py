ebedbc66f07f0ce8bba9a722cf7d6a9b5bcd6076
veilrain <v3i1r4in@gmail.com>
2024-05-13 16:26:00 -0700

    switch to flask

--- grab.py
+++ grab.py
@@ -1,10 +1,13 @@
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 from PIL import Image
 import time
 from mss import mss
+from flask import Flask, send_file
+from flask_cors import CORS
 
+port = 31245
+app = Flask(__name__)
+CORS(app)
 
 def grab_screen():
     # Capture entire screen
@@ -17,30 +20,19 @@ def grab_screen():
         # Convert to PIL/Pillow Image
         return Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
 
-PORT = 31245
-
-class PILHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+@app.route('/', methods=['GET'])
+def grab_endpoint():
+    try:
         # Create an image using PIL
         image = grab_screen()
         img_byte_arr = BytesIO()
         image.save(img_byte_arr, format='PNG')
         img_byte_arr = img_byte_arr.getvalue()
 
-        # Write response headers
-        self.send_response(200)
-        self.send_header('Content-type', 'image/png')
-        self.send_header('Content-length', len(img_byte_arr))
-        self.send_header('Access-Control-Allow-Origin', '*')  # This line enables CORS
-        self.end_headers()
+        return send_file(BytesIO(img_byte_arr), mimetype='image/png')
+    except Exception as e:
+        print(f'Error: {e}')
+        return 'Error: ' + str(e), 500
 
-        # Write Image Data
-        self.wfile.write(img_byte_arr)
-
-def run(server_class=HTTPServer, handler_class=PILHandler, port=8000):
-    server_address = ('0.0.0.0', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Serving at port {port}')
-    httpd.serve_forever()
-    
-run(port=PORT)
+if __name__ == '__main__':
+    app.run(port=port, debug=False, host='0.0.0.0')
